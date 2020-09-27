c9afd65b70f49b4d925b37dba94dbe5ee0e8b5cc
Andrey Petrov <3314099@mail.ru>
2020-09-27 15:36:06 +0300

    favicon

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const path = require('path')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
+const favicon = require('serve-favicon')
 
 const app = express()
 const port = 3000
 const indexRouter = require('./routes/index')
 
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use('/', indexRouter)
 
 app.use(logger('dev'))
